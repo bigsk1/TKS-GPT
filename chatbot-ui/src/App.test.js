4e8ac3c1a60ee4ef832fb053ee25f5ffafb28ae4
TKS <32640296+bigsk1@users.noreply.github.com>
2023-05-01 02:12:37 -0700

    ci tests update App.test.js

--- chatbot-ui/src/App.test.js
+++ chatbot-ui/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders GPT Ai Chat header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/GPT Ai Chat/i);
+  expect(headerElement).toBeInTheDocument();
 });
