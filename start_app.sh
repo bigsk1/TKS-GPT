a0a00f00f4260a308828c79ad8c9ce557270da53
TKS <32640296+bigsk1@users.noreply.github.com>
2023-05-01 18:38:11 -0700

    changes to devcontainer

--- start_app.sh
+++ start_app.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ ! -f ".env" ]; then
-    cp env.template .env
-    echo "Please enter your OpenAI API Key:"
-    read -r api_key
-    echo "OPENAI_API_KEY=$api_key" >> .env
+if [ ! -f .env ]; then
+    echo "Creating .env file from env.template"
+    cp .env.template .env
 fi
 
+echo "Enter your OpenAI API key:"
+read API_KEY
+echo "API_KEY=$API_KEY" >> .env
+
+echo "Starting Flask app..."
 flask run --host=0.0.0.0
