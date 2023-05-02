c629e41cd3d1ac38cdcde45405e359785a9790d8
TKS <32640296+bigsk1@users.noreply.github.com>
2023-05-01 20:55:53 -0700

    update devcontainer start

--- start_app.sh
+++ start_app.sh
@@ -6,9 +6,23 @@ if [ ! -f .env ]; then
     cp .env.template .env
 fi
 
+# Install Python dependencies if needed
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    . venv/bin/activate
+    pip install -r requirements.txt
+else
+    . venv/bin/activate
+fi
+
 echo "Enter your OpenAI API key:"
-read API_KEY
-echo "API_KEY=$API_KEY" >> .env
+read OPENAI_API_KEY
+
+# Remove any existing OPENAI_API_KEY line from the .env file
+grep -v "^OPENAI_API_KEY=" .env > .env.tmp && mv .env.tmp .env
+
+# Append the new OPENAI_API_KEY value to the .env file
+echo "OPENAI_API_KEY=$OPENAI_API_KEY" >> .env
 
 echo "Starting Flask app..."
 flask run --host=0.0.0.0
